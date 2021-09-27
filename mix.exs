5e3b4f147257a01b0226ef0b1e50a304fc94df07
William Normand <Faheetah@users.noreply.github.com>
2021-09-27 08:14:25 -0500

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GitAudit.MixProject do
   def project do
     [
       app: :git_audit,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
