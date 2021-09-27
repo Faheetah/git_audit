23e605b0737fe8265737bc661fd6382e23f06a8a
William Normand <Faheetah@users.noreply.github.com>
2021-09-27 08:13:02 -0500

    Add better help to command

--- lib/mix/tasks/git_audit.ex
+++ lib/mix/tasks/git_audit.ex
@@ -1,5 +1,25 @@
 defmodule Mix.Tasks.GitAudit do
-  @moduledoc "Recursively check for dirty and unpushed subdirectories for a given path"
+  @moduledoc """
+  Recursively traverses directories and determines if the directory has a .git subdirectory, then checks whether 
+  the repo is dirty or if there are any unpushed commits. The script will stop traversing when a directory 
+  contains a .git directory, to avoid unnecessarily recursing into vendored dependencies. Helpful to audit a 
+  local development environment for work that has not been pushed.
+
+  Run the git_audit task with a path. That's it. It will recursively audit the path.
+
+  mix git_audit PATH
+
+  For no colors, use the --no-ansi flag, i.e.
+
+  mix get_audit PATH --no-ansi
+  
+  The following commands are ran on each git directory to determine the status:
+  
+  dirty: git status --porcelain
+  unpushed: git log --branches --not --remotes --oneline
+  
+  If no condition matches, the directory is marked as ok
+  """
 
   use Mix.Task
 
